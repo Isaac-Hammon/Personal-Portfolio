43b8e906cc5f4a3e40dccee08dba5da85627ce4b
Isaac-Hammon <isaacboland23@gmail.com>
2026-01-28 12:12:52 -0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./projects.html", "./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
